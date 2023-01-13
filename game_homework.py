95db541ba80274da6cbcfed24fe7346cc416299e
elkorenkova <el_korenkova@mail.ru>
2023-01-13 19:45:47 +0300

    add new alg

--- game_homework.py
+++ game_homework.py
@@ -1,22 +1,49 @@
 import numpy as np
 
-def score_game(optimal_predict) -> int:
+def random_predict(number: int=1) -> int:
+    """Угадываем число ограничивая рамки подбора
+    
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    count = 0 
+    
+    min, max = 1, 100
+    predict_number = max / 2
+    while number != predict_number:
+        count+=1
+        # сужаем рамки поиска
+        
+        if number > predict_number: 
+            min = predict_number + 1
+        
+        elif number < predict_number: 
+            max = predict_number - 1
+
+        predict_number = round((max + min ) / 2) # разбиваем по полам новые рамки поиска      
+    return(count)
+    
   
-  """  За какое количество попыток в среднем за 1000 подходов угадывает наш алгоритм
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
 
     Args:
-        optimal_predict ([type]): функция угадывания
+        random_predict ([type]): функция угадывания
 
     Returns:
         int: среднее количество попыток
     """
-  count_ls = []
-  np.random.seed(1)  # фиксируем сид для воспроизводимости
-  random_array = np.random.randint(1, 101, size=(1000))  # загадали список онлайн переводчик чисел
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(10000))  # загадали список чисел
 
-  for number in random_array:
-    count_ls.append(optimal_predict(number))
+    for number in random_array:
+        count_ls.append(random_predict(number))
 
-  score = int(np.mean(count_ls))
-  print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
-score_game(optimal_predict)
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
+    
+score_game(random_predict)
